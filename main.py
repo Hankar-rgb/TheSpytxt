c7064122ce49de6898dc07b19def837b0f04102d
Casper Pepermans <casper.pepermans@telenet.be>
2020-10-28 17:27:21 +0000

    typo's and bug work

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if read == "y":
     print("Oke,", alias, "that is a solid name. I like it" )
     country = input ("So we know your alias, but what is your nationality? ")
     print("You know that", country, "is a bit of a risk to take? But I trust you on this one." )
-    hallway = print("We need to start moving. Yo need to go outside this room.")
+    hallway = print("We need to start moving. You need to go outside this room.")
     ans = input("You open the door and walk outside in to the hallway. Do you want go to the left side or the right? (left/right)? ")  
     if ans == "right":
       print("You bumb in to a house maid. She dropped something. You pick it up and say sorry to her.")
@@ -17,11 +17,11 @@ if read == "y":
       print("You are going towards the elevator...")  
     ans = input ("You notice somebody in a suit staring at you. You can walk away or you can run? (walk/run) ")
     if ans == "walk":
-      print("you walk to the elevator and press the button. The suited stranger opens the door accros your room.")
+      print("you manage to walk in to an open elevator and press the button. You look over your shoulder. The suited stranger opens the door accros your room. And walks in...")
+      
     elif ans == "run": 
       print("You start to run... he screams that you need to stop. You try to reach the staircase. But he shoots... you died.")
-    ans = input()
-    ans = input("Great, we are in the elevator! Do you want to the hotel lounge at first floor? Or do you want to visit the parking first? (first/parking")       
+       
   elif rules =="n":
       print("To bad. Now you die...")
     
